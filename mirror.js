56f4be9039fabdfdde2732679c8cfac1c54e81fa
dresser-github <56726595+dresser-github@users.noreply.github.com>
2020-05-19 00:09:49 +0300

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -23,7 +23,7 @@ client.on("ready", () => {
   client.user.setStatus("online");
   client.user.setPresence({
     game: {
-      name: "/help",
+      name: "ElementClan",
       type: "WATCHING",
       url:
         "https://www.youtube.com/channel/UCjjsEnIg5cDkw35UCD35bUA?view_as=subscriber"
@@ -54,20 +54,6 @@ client.on("guildMemberRemove", (member) => {
       .setTitle(membertag + " Left the server!")
   if(guild.systemChannel){
     guild.systemChannel.send(leftmsg);
-  }
-});
-
-
-//discord invites
-client.on('message', (message) => { 
-  if (message.content.includes('discord.gg/'||'discordapp.com/invite/')) { //if it contains an invite link 
-    const arrayOfUsersIds = ['595030276804050945', '472320129514864651', '632570913858125824'];
-
-    for (let i = 0; i < arrayOfUsersIds.length; i++) {
-    if (message.author.id === arrayOfUsersIds[i]) return;
-  };
-    message.delete() //delete the message 
-      .then(message.channel.send("<@" + message.author.id + ">" + " **Пожалуйста, не отправляйте приглашения в чат!**"))
   }
 });
 
@@ -129,32 +115,6 @@ client.on("message", async message => {
 }
 
 
- //rules
-   if(command === "rules") {
-    message.channel.send("**Русские правила:** ``/rulesru``\n**English rules:** ``/rulesen``");
-  }
-
-
-  //server rules ru
-    if(command === "rulesru") {
-    let server = new Discord.RichEmbed()
-      .setAuthor(message.guild.name, message.guild.iconURL)
-      .setDescription("**Правила сервера RU:**\n1. Запрещено оскорблять участников сервера.\n2. Запрещено спам, флуд в чате.\n3. Запрещено пиар других каналов, серверов.\n4. Запрещено материться в чате.\n5. Запрещено кричать, дуть в микрофон.\n6. Запрещено скидывать в текстовые каналы картинки с порнографией.\n7. Запрещено ставить оскорбительные, неадекватные ники.\n8. Запрещено выдавать себя за другого человека.\n9. Запрещено выдавать себя за администрацию.\n10. Запрещено оскорбительные родных.\n\n**Правила для Администрации:**\n1. Не злоупотреблять своими возможностями.\n2. Ведите себя адекватно.\n3. Помогайте участникам сервера.\n4. Не нарушайте правила сервера.")
-      .setColor("#00FFFF")
-    return message.channel.send(server);
-}
-
-
-  //server rules en
-  if (command === "rulesen") {
-   let rulesen = new Discord.RichEmbed()
-    .setAuthor(message.guild.name, message.guild.iconURL)
-    .setDescription("**Server rules EN:**\n1. Forbidden to insult server participants.\n2. Forbidden spam in chat.\n3. Forbidden to advertise other servers, channels.\n4. Forbidden to swear in chat.\n5. Forbidden to scream, blow into the microphone.\n6. Forbidden to pictures with pornography into text channels.\n7. Forbidden to put abusive, incorrect nicknames.\n8. Forbidden to impersonate another person.\n9. Forbidden to impersonate administration.\n10. Forbidden to insult relatives.\n\n**Rules for Administration:**\n1. Do not abuse your abilities.\n2. Behave appropriately.\n3. You must help server members.")
-    .setColor("#00FFFF")
-   return message.channel.send(rulesen);
-  }
-
-
   //random number command
     if(command === "random") {
     const lol = args.join(" ");
@@ -189,15 +149,6 @@ client.on("message", async message => {
   } 
 
 
- //server icon
-   if(command === "icon") {
-    const embed = new Discord.RichEmbed()
-                   .setImage(message.guild.iconURL)
-                   .setColor("#00FFFF")
-    message.channel.send(embed);
-  }
-
-
   //avatar command
     if(command === "avatar") {
        let user = message.mentions.users.first();
@@ -227,11 +178,11 @@ client.on("message", async message => {
     const deleteCount = parseInt(args[0], 10);
     
     if(!deleteCount || deleteCount < 2 || deleteCount > 1000)
-      return message.channel.send("Пожалуйста укажите сколько вы хотите удалить сообщений!");
+      return message.channel.send("Please indicate how many messages you want to delete.!");
     
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
-      .catch(error => message.channel.send(`Не удалось удалить сообщения из-за: ${error}`))
+      .catch(error => message.channel.send(`Error: ${error}`))
     message.channel.send("**You cleared " + deleteCount + " messages**").then(function(message) {
      message.delete(5000);
    });
